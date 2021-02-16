009c743c97d2793371f92cc28e2ea2f31acfb107
Jhostin Osorio <66564971+JhostinOsorio@users.noreply.github.com>
2021-02-15 20:13:23 -0500

    adding subdomain view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <hr>
+      <a href="http://dist.test">Ir al dominio principal</a> | 
+      <a href="http://dibal.dist.test">Ir al subdominio</a>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
+const host = window.location.host
+const parts = host.split('.')
+const domainLength = parts.length
+let routesByDomain = []
+
+if (domainLength === 2) {
+  routesByDomain = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+  ]
+} else if(domainLength === 3) {
+  routesByDomain = [
+    {
+      path: '/',
+      name: 'About',
+      component: About
+    }
+  ]
+}
+
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  ...routesByDomain
 ]
 
 const router = new VueRouter({
