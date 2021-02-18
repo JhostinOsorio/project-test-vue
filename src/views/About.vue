362641421a2216c371cdced41a2124bc87385edd
Jhostin Osorio <66564971+JhostinOsorio@users.noreply.github.com>
2021-02-17 20:26:10 -0500

    add name link

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>Esta es la pagina de subdominio</h1>
-    <router-link :to="{name: 'page-test'}"></router-link>
+    <router-link :to="{name: 'page-test'}">Page test</router-link>
   </div>
 </template>
